a6b4f5a6ff2ccbbc3e407108194a3b6e17eb338f
Offsekai <69424980+Offsekai@users.noreply.github.com>
2024-12-07 11:20:50 +0700

    update from GitHub

--- task1/task1/task1.cpp
+++ task1/task1/task1.cpp
@@ -13,4 +13,6 @@ int main() {
 
     cout << "Number " << number << " is bounded from below by even number: " << lower << ", and bounded from above by even number: " << upper; // Вывод результата
     return 0;
+
+    // 07.12.24
 }
