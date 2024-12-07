446bea6f717fb1853a2bbc094787f0ceaf1d6f93
Roman <wf.roma.03@gmail.com>
2024-12-07 11:13:16 +0700

    my first commit

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std; // Импорт пространства имен std (стандартной библиотеки C++).
+
+int main() {
+    float number; // Создание переменной number вещественного типа.
+    cin >> number; // Ввод данных в переменную number.
+
+    // Нижняя граница: округляем вниз и корректируем до ближайшего четного
+    int lower = int(floor(number)) - abs(int(floor(number)) % 2);
+
+    // Верхняя граница: округляем вверх и корректируем до ближайшего четного
+    int upper = int(ceil(number)) + abs(int(ceil(number)) % 2);
+
+    cout << "Number " << number << " is bounded from below by even number: " << lower << ", and bounded from above by even number: " << upper; // Вывод результата
+    return 0;
+}
